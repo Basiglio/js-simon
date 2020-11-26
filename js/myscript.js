ba41466c8b3577a8c672f95791a083811baa7dc7
Emilo <emilio.furnari87@gmail.com>
2020-11-26 16:15:22 +0100

    negato per js

--- js/myscript.js
+++ js/myscript.js
@@ -10,26 +10,57 @@ $(document).ready(
     var arrayNumeriPc = [];
     // CREO UN CONTENITORE PER I NUMERI DELl'UTENTE
     var arrayNumeriUtente = [];
+    // PUNTEGGIO
+    var punteggio = 0;
+    // NUMERI RICORDATI
+    var numeriGiusti = [];
+    // NUMERI NON RICORDATI
+    var numeriSbagliati = [];
+
 
  	  while (arrayNumeriPc.length < 5) {
       // GENERO NUMERO CASUALE
-   	 	randomNumberPc = randomNumber(1, 100);
-      arrayNumeriPc.push(randomNumberPc);
+   	 	var randomNumberPc = randomNumber(1, 100);
+      var check = isInArray(arrayNumeriPc, randomNumberPc)
+      if (check == false) {
+        arrayNumeriPc.push(randomNumberPc);
+      }
     }
-    console.log(arrayNumeriPc);
+    console.log("questi sono i numeri del pc" + arrayNumeriPc);
     alert("Memorizza questi numeri: " + arrayNumeriPc);
 
 
-    console.log(arrayNumeriUtente);
-
-    setTimeout(askNumber, 3000);
+    // IMPOSTO TIMER
+    setTimeout(askNumber, 1000);
     function askNumber() {
       while (arrayNumeriUtente.length < 5) {
         // GENERO NUMERO CASUALE
-        numeroUtente = parseInt(prompt("Inserisci un numero"));
-        arrayNumeriUtente.push(numeroUtente);
+        var numeroUtente = parseInt(prompt("Inserisci un numero"));
+        var check = isInArray(arrayNumeriUtente, numeroUtente)
+        if (check == true) {
+          alert("non puoi inserire lo stesso numero 2 volte")
+        } else {
+          arrayNumeriUtente.push(numeroUtente);
+        }
       }
     }
+    console.log("questi sono i tuoi numeri"arrayNumeriUtente);
+
+
+    // CONTROLLO I NUMERI
+    var gameCheck = isInArray(arrayNumeriPc, numeroUtente);
+    // SE IL NUMERO è NELL'ARRAY DEL PC
+    if (gameCheck == true) {
+      // PUSHA IL NUMERO NEL CONTENITORE NUMERI MEMORIZZATI ED AUMENTA IL PUNTEGGIO.
+      numeriGiusti.push(numeroUtente);
+      punteggio++;
+    } else {
+      // ALTRIMENTI PUSHA IL NUMERO NEL CONTENITORE SBAGLIATI
+      numeriSbagliati.push(numeroUtente)
+    }
+    console.log("i numeri che hai indovinato sono i seguenti " + numeriGiusti);
+    console.log("i numeri che hai dimenticato sono questi " + numeriSbagliati);
+    console.log("il tuo punteggio è " + punteggio);
   }
 );
 // MY FUNCTION----------------------------------------------
@@ -40,4 +71,16 @@ function randomNumber(min, max) {
   max = Math.floor(max);
   number = Math.floor(Math.random () * (max - min) +1);
   return number;
+}
+
+// FUNZIONE PER CERCARE IN UN ARRAY
+function isInArray(array, element) {
+	var result = false;
+	for (var i = 0; i < array.length; i++) {
+		if (element == array[i]) {
+			result = true;
+			return result;
+		}
+	}
+	return result;
 }
